0915af4aa5cc3d1728664c93346bdc8934931f0c
Oratakashi Nhamako <davidcreazy@gmail.com>
2020-07-10 11:58:54 +0700

    Optimaze Import

--- app/src/main/java/com/oratakashi/pendaftaran/ui/main/MainActivity.kt
+++ app/src/main/java/com/oratakashi/pendaftaran/ui/main/MainActivity.kt
@@ -6,7 +6,6 @@ import android.os.Bundle
 import android.view.View
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.content.ContextCompat
 import androidx.recyclerview.widget.ItemTouchHelper
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
